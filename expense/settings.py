2726cb6fc8d950efa4c875eb3cbdc0fcb2e14e59
mwiko <mwikosinyangwe@gmail.com>
2024-08-08 02:08:30 +0100

    ffff

--- expense/settings.py
+++ expense/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expenses',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/images/'
 
